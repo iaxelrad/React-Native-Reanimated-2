c15c090f4ee25576373818ae911a8855bdfc1040
Itamar Axelrad <itamara@aman.co.il>
2022-04-30 13:19:24 +0300

    Animate opacity withTiming

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+const SIZE = 100;
 
 const App = () => {
+  const progress = useSharedValue(1);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+    };
+  }, []);
+
+  useEffect(() => {
+    progress.value = withTiming(0.5, {duration: 5000});
+  }, []);
+
   return (
     <View style={styles.backgroundStyle}>
-      <Text>App.Js</Text>
+      <Animated.View
+        style={[
+          {height: SIZE, width: SIZE, backgroundColor: 'blue'},
+          reanimatedStyle,
+        ]}
+      />
     </View>
   );
 };
