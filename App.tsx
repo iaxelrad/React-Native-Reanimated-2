c8f5a156a59d52cc65f8a8f84a60f269c0f58aa3
Itamar Axelrad <itamara@aman.co.il>
2022-05-01 19:15:58 +0300

    Default master setup

--- App.tsx
+++ App.tsx
@@ -1,51 +1,17 @@
-import React, {useEffect} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-  withRepeat,
-  withTiming,
-} from 'react-native-reanimated';
-
-const SIZE = 100;
-
-const handleRotation = (progress: Animated.SharedValue<number>) => {
-  'worklet';
-  return `${progress.value * 2 * Math.PI}rad`;
-};
+import React from 'react';
+import {Text, StyleSheet, View, StatusBar} from 'react-native';
 
 const App = () => {
-  const progress = useSharedValue(1);
-  const scale = useSharedValue(2);
-
-  const reanimatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: progress.value,
-      borderRadius: (progress.value * SIZE) / 2,
-      transform: [{scale: scale.value}, {rotate: handleRotation(progress)}],
-    };
-  }, []);
-
-  useEffect(() => {
-    progress.value = withRepeat(withSpring(0.5), 3, true); // For infinite loop set 2nd parameter of withRepeat to -1
-    scale.value = withRepeat(withSpring(1), 3, true);
-  }, []);
-
   return (
-    <View style={styles.backgroundStyle}>
-      <Animated.View
-        style={[
-          {height: SIZE, width: SIZE, backgroundColor: 'blue'},
-          reanimatedStyle,
-        ]}
-      />
+    <View style={styles.container}>
+      <Text>Pinch Gesture Handler</Text>
+      <StatusBar barStyle="dark-content" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  backgroundStyle: {
+  container: {
     flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
