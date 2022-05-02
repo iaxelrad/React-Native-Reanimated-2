d02ddc3f7d88756f21ccb186f953c9dd0e10ca7b
Itamar Axelrad <itamara@aman.co.il>
2022-05-02 18:41:03 +0300

    Instagram Heart Animation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,39 @@
-import React, {useRef} from 'react';
-import {Dimensions, Image, StyleSheet, View} from 'react-native';
+import React, {useCallback, useRef} from 'react';
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  View,
+} from 'react-native';
 import {TapGestureHandler} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSpring,
+} from 'react-native-reanimated';
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 const App = () => {
+  const scale = useSharedValue(0);
+
   const doubleTapRef = useRef();
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: Math.max(scale.value, 0)}],
+    };
+  });
+
+  const onDoubleTap = useCallback(() => {
+    scale.value = withSpring(1, undefined, isFinished => {
+      if (isFinished) {
+        scale.value = withDelay(500, withSpring(0));
+      }
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -14,8 +44,18 @@ const App = () => {
           maxDelayMs={250}
           ref={doubleTapRef}
           numberOfTaps={2}
-          onActivated={() => console.log('DOUBLE_TAP')}>
-          <Image source={require('./assets/image.jpeg')} style={styles.image} />
+          onActivated={onDoubleTap}>
+          <Animated.View>
+            <ImageBackground
+              source={require('./assets/image.jpeg')}
+              style={styles.image}>
+              <AnimatedImage
+                source={require('./assets/heart.png')}
+                style={[styles.image, styles.heart, rStyle]}
+                resizeMode="center"
+              />
+            </ImageBackground>
+          </Animated.View>
         </TapGestureHandler>
       </TapGestureHandler>
     </View>
@@ -34,6 +74,15 @@ const styles = StyleSheet.create({
   image: {
     width: SIZE,
     height: SIZE,
+  },
+  heart: {
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 35,
+    shadowColor: 'black',
   },
 });
 
