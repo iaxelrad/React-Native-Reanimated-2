5b48e127cb1a618d729fdd13e0515b3902b1113d
Itamar Axelrad <itamara@aman.co.il>
2022-05-02 18:23:36 +0300

    The Double Tap Gesture

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Dimensions, Image, StyleSheet, View} from 'react-native';
+import {TapGestureHandler} from 'react-native-gesture-handler';
 
 const App = () => {
+  const doubleTapRef = useRef();
+
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/image.jpeg')} style={styles.image} />
+      <TapGestureHandler
+        waitFor={doubleTapRef}
+        onHandlerStateChange={() => console.log('SINGLE_TAP')}>
+        <TapGestureHandler
+          maxDelayMs={250}
+          ref={doubleTapRef}
+          numberOfTaps={2}
+          onActivated={() => console.log('DOUBLE_TAP')}>
+          <Image source={require('./assets/image.jpeg')} style={styles.image} />
+        </TapGestureHandler>
+      </TapGestureHandler>
     </View>
   );
 };
