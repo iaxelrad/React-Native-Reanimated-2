fb9a756ccdc16cb116f6aaf8fb4a5bb327e30a2c
Itamar Axelrad <itamara@aman.co.il>
2022-05-04 15:51:37 +0300

    The PanGestureHandler setup

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -3,15 +3,50 @@ import React, {FC} from 'react';
 import LinearGradient, {
   LinearGradientProps,
 } from 'react-native-linear-gradient';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 interface ColorPickerProps extends LinearGradientProps {}
+type ContextType = {
+  x: number;
+};
 
 const ColorPicker: FC<ColorPickerProps> = ({colors, start, end, style}) => {
+  const translateX = useSharedValue(0);
+
+  const panGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    ContextType
+  >({
+    onStart: (_, context) => {
+      context.x = translateX.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.x;
+    },
+    onEnd: event => {},
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: translateX.value}],
+    };
+  });
+
   return (
-    <View style={{justifyContent: 'center'}}>
-      <LinearGradient colors={colors} start={start} end={end} style={style} />
-      <View style={styles.picker} />
-    </View>
+    <PanGestureHandler onGestureEvent={panGestureEvent}>
+      <Animated.View style={{justifyContent: 'center'}}>
+        <LinearGradient colors={colors} start={start} end={end} style={style} />
+        <Animated.View style={[styles.picker, rStyle]} />
+      </Animated.View>
+    </PanGestureHandler>
   );
 };
 
