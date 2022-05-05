9a05512aeb2705e5376f0c279662943d9aea27a8
Itamar Axelrad <itamara@aman.co.il>
2022-05-05 19:46:09 +0300

    Translate Everywhere

--- components/SlidingCounter.tsx
+++ components/SlidingCounter.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 const ICON_SIZE = 20;
 
 const SlidingCounter = () => {
+  const translateX = useSharedValue(0);
+
+  const onPanGestureEvent =
+    useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
+      onStart: () => {},
+      onActive: event => {
+        translateX.value = event.translationX;
+        console.log(event.translationX);
+      },
+      onEnd: () => {
+        translateX.value = withSpring(0);
+      },
+    });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: translateX.value}],
+    };
+  });
+
   return (
-    <View style={styles.container}>
+    <View style={styles.button}>
       <Icon name="minus" size={ICON_SIZE} color="#ffffff" />
       <Icon name="close" size={ICON_SIZE} color="#ffffff" />
       <Icon name="plus" size={ICON_SIZE} color="#ffffff" />
-      <View style={styles.circleContainer}>
-        <View style={styles.circle} />
-      </View>
+      <PanGestureHandler onGestureEvent={onPanGestureEvent}>
+        <Animated.View style={[styles.circleContainer, rStyle]}>
+          <View style={styles.circle} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  button: {
     height: 70,
     width: 170,
     backgroundColor: '#111111',
