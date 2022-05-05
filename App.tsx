7e0667d35211e70fae2efd7c5a973e40d8ad376b
Itamar Axelrad <itamara@aman.co.il>
2022-05-05 18:05:35 +0300

    The PanGestureHandler

--- App.tsx
+++ App.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 const App = () => {
+  const translateX = useSharedValue(0);
+  const panGestureEvent =
+    useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
+      onActive: event => {
+        translateX.value = event.translationX;
+      },
+    });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: translateX.value}],
+    };
+  });
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Default App.tsx</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <PanGestureHandler onGestureEvent={panGestureEvent}>
+        <Animated.View style={[styles.animatedContainer, rStyle]} />
+      </PanGestureHandler>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1e1e23',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  animatedContainer: {
+    flex: 1,
     backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
