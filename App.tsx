79d49544b1ae5bffed9a45724401199f69d33d6e
Itamar Axelrad <itamara@aman.co.il>
2022-05-05 08:29:07 +0300

    Setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import {Text, StyleSheet, View, StatusBar} from 'react-native';
+import {Text, StyleSheet, View, StatusBar, Dimensions} from 'react-native';
+import Svg, {Circle} from 'react-native-svg';
+
+const BACKGROUND_COLOR = '#444b6f';
+const BACKGROUND_STROKE_COLOR = '#303858';
+const STROKE_COLOR = '#a6e1fa';
+
+const {width, height} = Dimensions.get('window');
+
+const CIRCLE_LENGTH = 1000;
+const CIRCLE_RADIUS = CIRCLE_LENGTH / (2 * Math.PI);
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Pinch Gesture Handler</Text>
-      <StatusBar barStyle="dark-content" />
+      <Svg
+        x={width / 2}
+        y={height / 2}
+        stroke={BACKGROUND_STROKE_COLOR}
+        strokeWidth={30}>
+        <Circle
+          cx={width / 2}
+          cy={height / 2}
+          r={CIRCLE_RADIUS}
+          stroke={STROKE_COLOR}
+          strokeWidth={15}
+        />
+      </Svg>
     </View>
   );
 };
@@ -13,7 +34,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: BACKGROUND_COLOR,
     alignItems: 'center',
     justifyContent: 'center',
   },
