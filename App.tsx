6f417776da529b8a8d856eb374922448a7ce62ec
Itamar Axelrad <itamara@aman.co.il>
2022-05-06 16:06:48 +0300

    Footer UI

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {StatusBar, StyleSheet, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import Animated, {
   useAnimatedScrollHandler,
   useSharedValue,
 } from 'react-native-reanimated';
 import Page from './components/Page';
 import {BACKGROUND_COLOR, PAGES} from './constants';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   const translateX = useSharedValue(0);
@@ -35,6 +36,22 @@ const App = () => {
           />
         ))}
       </Animated.ScrollView>
+      <View style={styles.footer}>
+        {/* Paginator */}
+        <View style={styles.fillCenter}>
+          {PAGES.map((page, index) => {
+            return <View style={styles.dot} />;
+          })}
+        </View>
+        {/* Text Container */}
+        <View style={styles.fillCenter}>
+          <Text style={styles.text}>View Board</Text>
+        </View>
+        {/* Icon Container */}
+        <View style={styles.fillCenter}>
+          <Icon name="arrowright" size={24} color="black" />
+        </View>
+      </View>
     </View>
   );
 };
@@ -45,6 +62,23 @@ const styles = StyleSheet.create({
     backgroundColor: BACKGROUND_COLOR,
   },
   scrollView: {flex: 1},
+  footer: {
+    height: 50,
+    marginBottom: 50,
+    flexDirection: 'row',
+  },
+  fillCenter: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 14,
+    textTransform: 'uppercase',
+    letterSpacing: 1.7,
+    fontWeight: '500',
+  },
+  dot: {},
 });
 
 export default App;
