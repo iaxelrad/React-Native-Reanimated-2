aaab2c3dcf8c450b3ccb2cdec3607433c318c176
Itamar Axelrad <itamara@aman.co.il>
2022-05-06 13:50:43 +0300

    Controlling the BottomSheet from the button

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import {StatusBar, StyleSheet, View} from 'react-native';
+import React, {useCallback, useRef} from 'react';
+import {StatusBar, StyleSheet, TouchableOpacity, View} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import BottomSheet from './components/BottomSheet';
+import BottomSheet, {BottomSheetRefProps} from './components/BottomSheet';
 
 const App = () => {
+  const ref = useRef<BottomSheetRefProps>(null);
+  const onPress = useCallback(() => {
+    const isActive = ref.current?.isActive();
+    if (isActive) {
+      ref?.current?.scrollTo(0);
+    } else {
+      ref?.current?.scrollTo(-200);
+    }
+  }, []);
+
   return (
     <GestureHandlerRootView style={styles.gestureRoot}>
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
-        <BottomSheet />
+        <TouchableOpacity style={styles.button} onPress={onPress} />
+        <BottomSheet ref={ref} />
       </View>
     </GestureHandlerRootView>
   );
@@ -19,6 +30,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#111',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    height: 50,
+    aspectRatio: 1,
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    opacity: 0.6,
   },
 });
 
--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {forwardRef, useCallback, useImperativeHandle} from 'react';
 import {Dimensions, StyleSheet, View} from 'react-native';
 import {Gesture, GestureDetector} from 'react-native-gesture-handler';
 import Animated, {
@@ -12,55 +12,76 @@ import Animated, {
 const {height: SCREEN_HEIGHT} = Dimensions.get('window');
 const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 50;
 
-const BottomSheet = () => {
-  const translateY = useSharedValue(0);
-
-  const scrollTo = useCallback((destination: number) => {
-    'worklet';
-    translateY.value = withSpring(destination, {damping: 50});
-  }, []);
-
-  const context = useSharedValue({y: 0});
-  const gesture = Gesture.Pan()
-    .onStart(() => {
-      context.value = {y: translateY.value};
-    })
-    .onUpdate(event => {
-      translateY.value = event.translationY + context.value.y;
-      translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y);
-    })
-    .onEnd(() => {
-      if (translateY.value > -SCREEN_HEIGHT / 3) {
-        scrollTo(0);
-      } else if (translateY.value < -SCREEN_HEIGHT / 1.5) {
-        scrollTo(MAX_TRANSLATE_Y);
-      }
-    });
-
-  useEffect(() => {
-    scrollTo(-SCREEN_HEIGHT / 3);
-  }, []);
-
-  const rBottomSheetStyle = useAnimatedStyle(() => {
-    const borderRadius = interpolate(
-      translateY.value,
-      [MAX_TRANSLATE_Y + 50, MAX_TRANSLATE_Y],
-      [25, 5],
-      Extrapolate.CLAMP,
-    );
-    return {
-      transform: [{translateY: translateY.value}],
-      borderRadius,
-    };
-  });
-  return (
-    <GestureDetector gesture={gesture}>
-      <Animated.View style={[styles.container, rBottomSheetStyle]}>
-        <View style={styles.line} />
-      </Animated.View>
-    </GestureDetector>
-  );
+export type BottomSheetProps = {};
+export type BottomSheetRefProps = {
+  scrollTo: (destination: number) => void;
+  isActive: () => boolean;
 };
+
+const BottomSheet = forwardRef<BottomSheetRefProps, BottomSheetProps>(
+  ({}, ref) => {
+    const translateY = useSharedValue(0);
+    const active = useSharedValue(false);
+
+    const scrollTo = useCallback((destination: number) => {
+      'worklet';
+      active.value = destination !== 0;
+
+      translateY.value = withSpring(destination, {damping: 50});
+    }, []);
+
+    const isActive = useCallback(() => {
+      'worklet';
+      return active.value;
+    }, []);
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        scrollTo,
+        isActive,
+      }),
+      [scrollTo, isActive],
+    );
+
+    const context = useSharedValue({y: 0});
+    const gesture = Gesture.Pan()
+      .onStart(() => {
+        context.value = {y: translateY.value};
+      })
+      .onUpdate(event => {
+        translateY.value = event.translationY + context.value.y;
+        translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y);
+      })
+      .onEnd(() => {
+        if (translateY.value > -SCREEN_HEIGHT / 3) {
+          scrollTo(0);
+        } else if (translateY.value < -SCREEN_HEIGHT / 1.5) {
+          scrollTo(MAX_TRANSLATE_Y);
+        }
+      });
+
+    const rBottomSheetStyle = useAnimatedStyle(() => {
+      const borderRadius = interpolate(
+        translateY.value,
+        [MAX_TRANSLATE_Y + 50, MAX_TRANSLATE_Y],
+        [25, 5],
+        Extrapolate.CLAMP,
+      );
+      return {
+        transform: [{translateY: translateY.value}],
+        borderRadius,
+      };
+    });
+    return (
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={[styles.container, rBottomSheetStyle]}>
+          <View style={styles.line} />
+        </Animated.View>
+      </GestureDetector>
+    );
+  },
+);
 
 export default BottomSheet;
 
